3a1249abac951d1d8d0246da1c65d6a1dceb5801
Chris Jager <cjager.com@gmail.com>
2025-10-08 21:05:58 +0200

    route to js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
-// ESM ok on Vercel Node runtime
 import { buildServer } from '../dist/app.js'
-
 const app = buildServer()
-
 export default async function handler(req, res) {
   await app.ready()
   app.server.emit('request', req, res)
