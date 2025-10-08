8337ef7aa1d7b802c1f83dc62ac4216c31af2459
Chris Jager <cjager.com@gmail.com>
2025-10-08 21:20:51 +0200

    mess around swager docs

--- api/index.js
+++
@@ -1,6 +0,0 @@
-import { buildServer } from '../dist/app.js'
-const app = buildServer()
-export default async function handler(req, res) {
-  await app.ready()
-  app.server.emit('request', req, res)
-}
--- src/routes/public/route.ts
+++
@@ -1,4 +0,0 @@
-import { FastifyInstance } from 'fastify'
-export default async function r(app: FastifyInstance) {
-  app.get('/', async () => ({ name: 'WUB Fulfillment API', docs: '/docs', api: '/api/v1' }))
-}
