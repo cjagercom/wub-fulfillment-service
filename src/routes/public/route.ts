94424d8bad86ea0b099464b8bf058cc6d28c7784
Chris Jager <cjager.com@gmail.com>
2025-10-08 21:27:07 +0200

    mess around with swagger docs location again

--- src/routes/public/route.ts
+++ src/routes/public/route.ts
@@ -1,4 +1,4 @@
 import { FastifyInstance } from 'fastify'
 export default async function r(app: FastifyInstance) {
-  app.get('/', async () => ({ name: 'WUB Fulfillment API', docs: '/docs', api: '/api/v1' }))
+  app.get('/', async () => ({ name: 'WUB Fulfillment API', docs: '/', api: '/api/v1' }))
 }
