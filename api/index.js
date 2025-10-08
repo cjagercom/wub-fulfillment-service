792c77b87f755f466007832dc62f52d274c008b7
Chris Jager <cjager.com@gmail.com>
2025-10-08 21:25:58 +0200

    revert

---
+++ api/index.js
@@ -0,0 +1,6 @@
+import { buildServer } from '../dist/app.js'
+const app = buildServer()
+export default async function handler(req, res) {
+  await app.ready()
+  app.server.emit('request', req, res)
+}
